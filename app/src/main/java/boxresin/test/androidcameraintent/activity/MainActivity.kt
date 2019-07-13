b8ea580efc1de6f46e72c432b2de2709089429ce
Minsuk Eom <boxresin@gmail.com>
2019-07-13 14:02:27 +0900

    메인 액티비티 추가

---
+++ app/src/main/java/boxresin/test/androidcameraintent/activity/MainActivity.kt
@@ -0,0 +1,11 @@
+package boxresin.test.androidcameraintent.activity
+
+import android.os.Bundle
+import boxresin.test.androidcameraintent.R
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.setContentView(R.layout.activity_main)
+    }
+}
